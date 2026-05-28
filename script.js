4da640581d51bcf6b64e1c0c361af5773a804d8f
Davkayson <horginnidavid@gmail.com>
2026-05-28 12:51:13 +0100

    Acheived the dark and light mode of the background

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-const sun = document.querySelector('.sun')
-const moon = document.querySelector('.moon')
+const themeToggleBtn = document.getElementById('theme-toggle');
 
-sun.addEventListener('click', () => {
-  sun.style.display = 'none';
-})
+themeToggleBtn.addEventListener('click', () => {
+  const currentTheme = document.body.getAttribute('data-theme')
+
+  if (currentTheme === 'dark') {
+    document.body.removeAttribute('data-theme')
+  } else {
+    document.body.setAttribute('data-theme', 'dark')
+  }
+});
+
+
+//When the browser loads the light mode should be on by default without the sun icon
+
+// when i clik on the moon it turns to dark mode
+
+// when i click on the sun, it show turn back to light mode.
+
+//when i click on the all button all the sections should show.
+
+//when i click on the active the ones with the check buttons shows.
+
+//when i click on the inactive the one with the unchecked buttons shows.
